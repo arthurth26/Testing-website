4b693ae5603ee6eec6d6178f4c355207f7a28edb
Arthur Lee <72230266+arthurth26@users.noreply.github.com>
2025-07-02 01:07:37 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 
 export default defineConfig({
-  base: "/Testing-website/",
+  base: '/Testing-website/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
