5f3f8f28c1df8e81a020d7e63075fcd80fd8361e
Arthur Lee <72230266+arthurth26@users.noreply.github.com>
2025-07-03 00:58:20 +0800

    Update nav_bar.tsx

--- app/nav_bar/nav_bar.tsx
+++ app/nav_bar/nav_bar.tsx
@@ -1,12 +1,12 @@
-import {NavLink, useLocation} from "react-router";   
-import { HouseHome, MagnifyingGlass } from "@vectopus/atlas-icons-react";
+import {NavLink} from "react-router";   
+import * as Icon from "@vectopus/atlas-icons-react";
 import React from "react";
 
 export function Nav () {
     return (
         <div className="flex justify-around bg-gray-800 p-4">
             <NavLink to="/" className="flex-none">
-                <HouseHome size={24} className="text-gray-100"/>
+                <Icon.HouseHome size={24} className="text-gray-100"/>
             </NavLink>          
             <div className="items-center mx-auto place-items-center"> 
                 <NavLink to="/" className={({isActive}) => isActive ? "text-teal-500 mx-4 pl-20" : "text-gray-200 mx-4 pl-20"}>
